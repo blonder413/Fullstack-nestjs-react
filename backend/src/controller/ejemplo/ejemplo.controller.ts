f76fd25293d4551f634e3efa7c92b2485cf85efd
Jonathan Morales <blonder413@gmail.com>
2025-01-11 15:44:33 -0500

    nestjs: endpoints con distintos métodos http

--- backend/src/controller/ejemplo/ejemplo.controller.ts
+++ backend/src/controller/ejemplo/ejemplo.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('ejemplo')
 export class EjemploController {
   @Get()
   index() {
     return 'Método Get';
+  }
+
+  @Post()
+  create() {
+    return 'Método Post';
+  }
+
+  @Put()
+  update() {
+    return 'Método put';
+  }
+
+  @Delete()
+  destroy() {
+    return 'Método delete';
   }
 }
