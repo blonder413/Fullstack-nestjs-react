b8308c910536be8000f099cec01e6390418454fa
Jonathan Morales <blonder413@gmail.com>
2025-01-18 15:31:31 -0500

    react: páginas principales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDom from "react-dom/client";
 import { Home } from "./paginas/Home";
+import { About } from "./paginas/About";
 import { Frontend } from "./componentes/Frontend";
 
 import "../public/style.css";
+import { RecetaDetalle } from "./paginas/RecetaDetalle";
+import { Contacto } from "./paginas/Contacto";
+import { Recetas } from "./paginas/Recetas";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        children: [{ element: <Home />, index: true }],
+        children: [
+            { element: <Home />, index: true },
+            { element: <About />, path: "/nosotros" },
+            { element: <Recetas />, path: "/recetas" },
+            { element: <RecetaDetalle />, path: "/recetas/detalle" },
+            { element: <Contacto />, path: "/contacto" },
+        ],
         element: <Frontend />,
     },
 ]);
---
+++ frontend/src/paginas/RecetaDetalle.jsx
@@ -0,0 +1,5 @@
+export const RecetaDetalle = () => {
+  return (
+    <div>RecetaDetalle</div>
+  )
+}
