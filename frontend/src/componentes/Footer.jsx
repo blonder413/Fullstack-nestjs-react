7b8ba1655b234bf024e7fbdf70969dc5e97147c7
Jonathan Morales <blonder413@gmail.com>
2025-01-18 11:54:10 -0500

    react: componente footer

---
+++ frontend/src/componentes/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer className="py-5 text-center text-body-secondary bg-body-tertiary">
+            <p>
+                Todos los derechos reservados 2025 | Desarrollado por{" "}
+                <a
+                    href="https://blonder413.wordpress.com/"
+                    title="Blonder413"
+                    target="_blank"
+                >
+                    Blonder413
+                </a>
+            </p>
+        </footer>
+    );
+};
