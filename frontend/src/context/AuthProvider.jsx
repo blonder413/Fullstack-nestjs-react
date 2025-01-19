6957170e5bad0682805d998ab54ffdcd28d41a7d
Jonathan Morales <blonder413@gmail.com>
2025-01-19 18:32:54 -0500

    react: implementación de localStorage

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -2,11 +2,29 @@ import { createContext, useState } from "react";
 
 const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState();
-    const handleValidaLogin = () => {};
+    const [auth, setAuth] = useState(
+        localStorage.getItem("recetas_flaites_id") != null
+    );
+    const [authId, setAuthId] = useState("");
+    const [authNombre, setAuthNombre] = useState("");
+    const [authToken, setAuthToken] = useState("");
+    const handleValidaLogin = () => {
+        if (!auth && localStorage.getItem("recetas_flaites_id") == null) {
+            window.location = "/login";
+        }
+        setAuth(true);
+        setAuthId(localStorage.getItem("recetas_flaites_id"));
+        setAuthNombre(localStorage.getItem("recetas_flaites_nombre"));
+        setAuthToken(localStorage.getItem("recetas_flaites_token"));
+    };
+    const handleIniciarSesion = (id, nombre, token) => {
+        localStorage.setItem("recetas_flaites_id", id);
+        localStorage.setItem("recetas_flaites_nombre", nombre);
+        localStorage.setItem("recetas_flaites_token", token);
+    };
     return (
         <AuthContext.Provider
-            value={{ auth, handleValidaLogin }}
+            value={{ auth, handleValidaLogin, handleIniciarSesion }}
         ></AuthContext.Provider>
     );
 };
